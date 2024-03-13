ca0790e2ce116d21460c3aefa2111fcac380f68a
Chandra Shekhar <chandradot99@gmail.com>
2024-03-13 12:32:20 +0530

    add chess page and layout

---
+++ src/app/chess/page.tsx
@@ -0,0 +1,9 @@
+export default function ChessPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        Chess Home Page
+      </div>
+    </main>
+  );
+}
